392b8669f2b26db7e79a57a0633ddc2201889175
Saul Hirner <saulhirner@gmail.com>
2022-12-05 13:54:42 -0500

    pink

---
+++ script.js
@@ -0,0 +1,7 @@
+
+//const ima = document.getElementById("a");
+//const back = document.getElementById("body");
+
+//ima.addEventListener("mouseover", (event) => {
+//    back.style.backgroundColor = "purple"
+//});
